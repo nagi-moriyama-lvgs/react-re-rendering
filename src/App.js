60fc83636a33e9c8fd5dad6b72ea294ecefc30ba
nagi-moriyama-lvgs <nagi.moriyama@leverages.jp>
2021-06-26 13:56:27 +0900

    レンダリングの再現

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const onClickCountUp = () => setCount(count + 1);
+  console.log("レンダリングが走りました。");
+
   return (
     <div className="App">
       <h1>React re-rendering</h1>
+      <button onClick={onClickCountUp}>Count UP</button>
+      {count}
     </div>
   );
-}
+};
 
 export default App;
